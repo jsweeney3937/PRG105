09862f7245c6b2047cc14e6d8406e81a1f5dfe46
John Sweeney <jsweeney3937@students.mchenry.edu>
2017-06-28 17:44:13 -0500

    Add files via upload

---
+++ wk4_Average of Numbers.py
@@ -0,0 +1,32 @@
+# opens file named numbers.txt and reads it only
+file1 = open('numbers.txt', 'r')
+# initializes 0 as the average
+av = 0
+# initializes 0 as the total of all numbers in file numbers.txt
+totalNum = 0
+# initializes 0 as the number of numbers in file numbers.txt
+count = 0
+
+
+# defines function main
+def main():
+    # declares global variables
+    global totalNum
+    global count
+    global av
+    # loop that reads each value line by line
+    for line in file1:
+        # adds the value of each number to total of numbers and reassigns its value
+        totalNum += int(line)
+        # adds 1 to count
+        count += 1
+    # calculates the average of total value of numbers from numbers.txt and divides it by
+    # count, the total number of values in numbers.txt
+    av = (totalNum / count)
+    # prints the total value, number of items in file, and average
+    print("The total of the values in 'numbers.txt' is:", totalNum)
+    print("The number of values in 'numbers.txt' is:", count)
+    print("The average of the numbers in 'numbers.txt' is: ", av)
+    file1.close()
+# calls function main
+main()
